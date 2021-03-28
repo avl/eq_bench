9a6c6035b86579e0e10e0ea7358ce77f32174a0d
Anders Musikka <anders@andersmusikka.se>
2021-03-28 12:59:22 +0200

    also check case of failing compare

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 #![feature(test)]
 #[allow(soft_unstable)]
 
+/*
+Output of cargo bench (on AMD 3900X):
+
+running 6 tests
+test bench_baseline_different                        ... bench:         300 ns/iter (+/- 2)
+test bench_baseline_equal                            ... bench:       1,170 ns/iter (+/- 18)
+test bench_manual_compare_non_shortcircuit_different ... bench:         942 ns/iter (+/- 10)
+test bench_manual_compare_non_shortcircuit_equal     ... bench:       1,165 ns/iter (+/- 7)
+test bench_u128_transmute_different                  ... bench:         475 ns/iter (+/- 3)
+test bench_u128_transmute_equal                      ... bench:         470 ns/iter (+/- 6)
+
+
+
+ */
+
 extern crate test;
 use test::Bencher;
 
@@ -31,7 +46,7 @@ pub fn u128_transmute_compare(a:&Blueprint, b:&Blueprint) -> bool {
 }
 
 #[bench]
-fn bench_baseline(b:&mut Bencher) {
+fn bench_baseline_equal(b:&mut Bencher) {
     let obj = Blueprint {
         fuel_tank_size: 0,
         payload: 0,
@@ -41,11 +56,14 @@ fn bench_baseline(b:&mut Bencher) {
     };
     let obj1s = vec![obj;1000];
     let obj2s = vec![obj;1000];
-    b.iter(||obj1s==obj2s)
+
+    b.iter(||{
+        obj1s.iter().zip(obj2s.iter()).all(|(a,b)| a == b)
+    })
 }
 
 #[bench]
-fn bench_manual_compare_non_shortcircuit(b:&mut Bencher) {
+fn bench_manual_compare_non_shortcircuit_equal(b:&mut Bencher) {
     let obj = Blueprint {
         fuel_tank_size: 0,
         payload: 0,
@@ -55,6 +73,7 @@ fn bench_manual_compare_non_shortcircuit(b:&mut Bencher) {
     };
     let obj1s = vec![obj;1000];
     let obj2s = vec![obj;1000];
+
     b.iter(||{
         obj1s.iter().zip(obj2s.iter()).all(|(a,b)| manual_compare_non_shortcircuit(a, b))
     })
@@ -62,7 +81,7 @@ fn bench_manual_compare_non_shortcircuit(b:&mut Bencher) {
 
 
 #[bench]
-fn bench_u128_transmute(b:&mut Bencher) {
+fn bench_u128_transmute_equal(b:&mut Bencher) {
     let obj = Blueprint {
         fuel_tank_size: 0,
         payload: 0,
@@ -72,10 +91,68 @@ fn bench_u128_transmute(b:&mut Bencher) {
     };
     let obj1s = vec![obj;1000];
     let obj2s = vec![obj;1000];
+
     b.iter(||{
         obj1s.iter().zip(obj2s.iter()).all(|(a,b)| u128_transmute_compare(a, b))
     })
 }
+
+#[bench]
+fn bench_baseline_different(b:&mut Bencher) {
+    let mut obj = Blueprint {
+        fuel_tank_size: 0,
+        payload: 0,
+        wheel_diameter: 0,
+        wheel_width: 0,
+        storage: 0
+    };
+    let obj1s = vec![obj;1000];
+    obj.fuel_tank_size=1;
+    let obj2s = vec![obj;1000];
+
+    b.iter(||{
+        obj1s.iter().zip(obj2s.iter()).any(|(a,b)| a == b)
+    })
+}
+
+#[bench]
+fn bench_manual_compare_non_shortcircuit_different(b:&mut Bencher) {
+    let mut obj = Blueprint {
+        fuel_tank_size: 0,
+        payload: 0,
+        wheel_diameter: 0,
+        wheel_width: 0,
+        storage: 0
+    };
+    let obj1s = vec![obj;1000];
+    obj.fuel_tank_size=1;
+    let obj2s = vec![obj;1000];
+
+    b.iter(||{
+        obj1s.iter().zip(obj2s.iter()).any(|(a,b)| manual_compare_non_shortcircuit(a, b))
+    })
+}
+
+
+#[bench]
+fn bench_u128_transmute_different(b:&mut Bencher) {
+    let mut obj = Blueprint {
+        fuel_tank_size: 0,
+        payload: 0,
+        wheel_diameter: 0,
+        wheel_width: 0,
+        storage: 0
+    };
+    let obj1s = vec![obj;1000];
+    obj.fuel_tank_size=1;
+    let obj2s = vec![obj;1000];
+
+    b.iter(||{
+        obj1s.iter().zip(obj2s.iter()).any(|(a,b)| u128_transmute_compare(a, b))
+    })
+}
+
+
 
 fn main() {
 }
